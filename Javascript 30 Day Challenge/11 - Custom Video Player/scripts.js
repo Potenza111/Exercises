4ffbdb7312fc1bc651db7f5c268a46c65b9e805e
Daneel van Tonder <dnlvantonder@gmail.com>
2020-03-10 20:35:58 +0200

    done

---
+++ Javascript 30 Day Challenge/11 - Custom Video Player/scripts.js
@@ -0,0 +1,59 @@
+// Get Our Elements
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const toggle = document.querySelector('.toggle');
+const skipButtons = document.querySelectorAll('[data-skip]');
+const ranges = document.querySelectorAll('.player__slider');
+
+// Build Funtions
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+function skip() {
+  console.log('skipping');
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+
+  video.currentTime = scrubTime;
+
+  console.log(e);
+}
+
+// Hook Up eventListners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mosuedown = false;
+
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', e => mosuedown && scrub(e));
+progress.addEventListener('mousedown', () => (mosuedown = true));
+progress.addEventListener('mouseup', () => (mosuedown = false));
